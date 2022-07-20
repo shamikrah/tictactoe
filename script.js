9f598e224f505cdb9a8b690f4f4d7777465ddf92
Shamik Rahman <shamikrah@gmail.com>
2022-07-20 21:08:02 +1000

    Add some styling to landing page and form modal

---
+++ script.js
@@ -0,0 +1,10 @@
+// Player factory that creates players
+const playerFactory = (name) => {
+    return {name};
+}
+
+// Game board module 
+const gameBoard = (() => {
+    board = [];
+    return {};
+})();
